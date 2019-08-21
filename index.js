0ea8008084d42da2bdb7247b8f60d4572907f4f8
fortunate-commit-9523 <alexiscash97@gmail.com>
2019-08-21 21:44:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 
 function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
   
+  for (var i = 0; i <= musicians.length; i++) {
+    beatles.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return beatles;
 }
