1fb2be8da5541afc7a1f01d9494e1f3881d0adf5
fortunate-commit-9523 <alexiscash97@gmail.com>
2019-08-21 21:46:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
 function theBeatlesPlay(musicians, instruments) {
   let beatles = [];
